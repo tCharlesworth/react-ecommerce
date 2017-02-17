36dda194526bc9aaeac0d5fba740548c1de98022
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-02-16 17:08:59 -0700

    hooking up register form.

--- app/utils/publicUtils.js
+++ app/utils/publicUtils.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
+const URL = 'http://localhost:3001'
+
 let publicUtils = {
   getProducts() {
-    return axios.get('http://localhost:3001/api/products').then((response) => {
+    return axios.get(URL+'/api/products').then((response) => {
       return response.data;
     }).catch((err) => {
       console.warn('Unable to retrieve products.');
+    });
+  },
+
+  createUser(data) {
+    return axios.post(URL+'/api/user').then((response) => {
+      return response.data;
+    }).catch((err) => {
+      console.warn('Unable to register user');
     });
   }
 };
--- app/views/register.js
+++ app/views/register.js
@@ -5,29 +5,41 @@ import { Link } from 'react-router';
 import PageTitle from '../components/pageTitle.js';
 
 class RegisterView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            confirmEmail: '',
+            password: '',
+            newsletter: true,
+            terms: false
+        };
+    }
+    tryRegister() {
+        console.log('clicked register')
+    }
     render() {
         return (
             <div>
                 <PageTitle text="Register" />
                 <div className="text-center">
                     <div>
-                        <input type="text" placeholder="username" />
-                        <input type="email" placeholder="email" />
-                        <input type="email" placeholder="confirm email" />
-                        <input type="password" placeholder="password" />
+                        <input type="email" placeholder="email" value={this.state.email} />
+                        <input type="email" placeholder="confirm email" value={this.state.confirmEmail} />
+                        <input type="password" placeholder="password" value={this.state.password} />
                         <div className="text-left">
                             <div>
-                                <input type="checkbox" />
+                                <input type="checkbox" checked={this.state.newsletter} />
                                 <label>I would like to receive the newsletter</label>
                             </div>
                             <div>
-                                <input type="checkbox" />
+                                <input type="checkbox" checked={this.state.terms} />
                                 <label>I agree to the terms & conditions</label>
                             </div>
                         </div>
                     </div>
                     <br/>
-                    <button>Register</button><br/><br/>
+                    <button onClick={ this.tryRegister.bind(this) }>Register</button><br/><br/>
                     <small>Already have an account? <Link to="/login">Login</Link></small>
                 </div>
             </div>
