6522d842b6f2568f8fdfca2e45f2481755a23502
Tim Charlesworth <tim_charlesworth@outlook.com>
2017-02-22 17:24:08 -0700

    Carousel now loads images. Selectors work and have some basic ugly styles.

--- app/components/carousel.js
+++ app/components/carousel.js
@@ -5,25 +5,41 @@ class CarouselComponent extends React.Component {
     super(props);
     this.state = {
       selectedImage: null,
+      selectedIndex: 0,
       defaultImage: 'https://c1.staticflickr.com/1/186/382004453_f4b2772254.jpg',
       images: props.images || []
     };
-    console.log('images: ', props);
-  }
-  updateProps(props) {
-    console.log('Got Props: ', props);
+    // console.log('images: ', props);
+    this.loadme()
   }
   handleSelectImage(idx) {
     console.log('Select Image: ', idx);
+    this.setState({
+      selectedImage: this.state.images[idx] || this.state.defaultImage,
+      selectedIndex: idx
+    });
+  }
+  loadme(images) {
+    if(images && images.length) {
+      this.setState({
+        images: images,
+        selectedImage: images[0],
+        selectedIndex: 0
+      });
+    } else {
+      window.setTimeout(() => {
+        this.props.load(this.loadme.bind(this));
+      }, 200);
+    }
   }
   render() {
     return (
       <div className="carousel">
         <img className="carousel-image" src={this.state.selectedImage || this.state.defaultImage} />
-        <div>
+        <div className="selectors">
           {this.state.images.map((img, idx) => {
             return (
-              <span className="carousel-selector" onClick={this.handleSelectImage.bind(this, idx)}></span>
+              <span key={idx} className={"carousel-selector"+ (idx == this.state.selectedIndex ? " carousel-selector-active" : "")} onClick={this.handleSelectImage.bind(this, idx)}></span>
             );
           })}
         </div>
--- app/views/product.js
+++ app/views/product.js
@@ -9,7 +9,8 @@ class ProductView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: {}
+            product: {},
+            images: []
         };
     }
     componentWillMount() {
@@ -18,17 +19,27 @@ class ProductView extends React.Component {
     loadProduct() {
         console.log('loaded id: ', this.props.params.productId);
         PublicUtils.getProduct(this.props.params.productId).then((product) => {
-            this.setState({product: product});
+            this.setState({
+                product: product,
+                images: product.images
+            });
         });
+    }
+    testFunc(func) {
+        func(this.state.images);
     }
     render() {
         return (
             <div>
                 <PageTitle text={ this.state.product.name || "No Name" } />
                 <hr />
-                <CarouselComponent images={this.state.images} />
+                <CarouselComponent images={this.state.images} load={this.testFunc.bind(this)} />
                 <p>Price: {this.state.product.price}</p>
                 <p>Description: {this.state.product.description}</p>
+                {/*<p>Images: {this.state.images.map((img, idx) => {
+                    return (<li key={idx}>{img}</li>)
+                    })}
+                </p>*/}
             </div>
         );
     }
